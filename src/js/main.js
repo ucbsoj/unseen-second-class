5e3c750f766f7474d00c7b2551603c70972e4220
Alex NIeves <alexdnieves5@gmail.com>
2019-04-26 18:30:23 -0700

    Website updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
 import $ from "jquery"; // importing jQuery, you can delete if not needed
 require("./lib/social"); // Twitter intent JS
+import Flickity from "flickity";
 
+
+
+
+var flky = new Flickity( '.carousel', {});
 
 
 //var imgReveal = $('#top-quote').offset().top;
